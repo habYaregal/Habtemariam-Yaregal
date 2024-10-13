897b23101a62efd7999e72b1696d36cb2c06dcf0
habYaregal <habtemariamyaregal@gmail.com>
2024-10-13 21:13:43 +0300

    customizing the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,41 @@
-const Home =()=>{
-    return(
+import React, { useState, useEffect } from 'react';
+import '../index.css';
+import background from '../assets/background.jpg';
+
+const Home = () => {
+    const [showFirstText, setShowFirstText] = useState(true);
+    const [isVisible, setIsVisible] = useState(true); 
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsVisible(false); 
+            setTimeout(() => {
+                setShowFirstText((prev) => !prev);
+                setIsVisible(true);
+            }, 500);
+        }, 2500);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
         <>
-            <h1>This is HomePage</h1>
+            <div
+                className="h-screen bg-cover bg-center flex flex-col justify-center items-center"
+                style={{ backgroundImage: `url(${background})` }}
+            >
+                <h1
+                    className={`text-center text-[80px] font-jacky text-[#FFFFFF] transition-opacity duration-500 ${
+                        isVisible ? 'opacity-100' : 'opacity-0'
+                    }`}
+                >
+                    {showFirstText
+                        ? 'Welcome to Habtemariam Yaregal Portfolio'
+                        : 'Click anywhere to continue'}
+                </h1>
+            </div>
         </>
     );
-
-}
+};
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,41 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          800: '#5B3B16', 
+          900: '#4A2F12', 
+        },
+        redish:{
+          500:'#6A2F27'
+        },
+        pencil:{
+          500:'#333333'
+        },
+        black:{
+          500:'#000000'
+        }
+      },
+      fontFamily: {
+        crafty: ['"Crafty Girls"', 'cursive'],
+        crimson: [' "Crimson Pro"', 'serif'],
+        lora: [' "Lora"', 'serif'],
+        railway:['"Raleway"', 'sans-serif'],
+        pirata:['"Pirata One"', 'system-ui'],
+        pixel:['"Pixelify Sans"', 'serif'],
+        jacky:['"Jacquard 24"', 'system-ui']
+      },
+      fontSize: {
+        '16px': '18px',
+        'head': '64px',
+        'par':'18px',
+        'qoute':'20px'
+        },
+      height:{
+        'all' : '100hv'
+      }  
+    },
   },
   plugins: [],
 }
